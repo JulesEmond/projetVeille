0a3d42032ce36058730bffb9f54287f106f536d5
Jules Emond <julesemond@outlook.com>
2021-09-15 18:27:21 -0400

    Reparation du frontend (creation d'un nouveau projet)

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  
+  validMessage:string = '';
+  loginForm = new FormGroup({
+    courriel : new FormControl('', Validators.required),
+    motDePasse : new FormControl('', Validators.required)
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onSubmit(){
+    
+  }
 }
